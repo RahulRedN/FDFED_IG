724b542b9dc949ef5386383082a928cfeba327bd
Rahul <rahul.red1801@gmail.com>
2023-11-26 00:52:33 +0530

    jobseeker home update

--- src/components/JobSeeker/Navbar.jsx
+++ src/components/JobSeeker/Navbar.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Link } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
 
 import classes from "./JobSeeker.module.css";
 import { CgProfile } from "react-icons/cg";
+import { useStatStyles } from "@chakra-ui/react";
 
 const Navbar = ({ navClass }) => {
+  const { pathname } = useLocation();
+
+  const isLinkActive = (path) => {
+    return pathname === path;
+  };
+
   return (
-    <nav className={navClass}>
-      <span>Inspiring Go</span>
-      <ul className={classes.items}>
-        <li>
-          <Link to="">HOME</Link>
-        </li>
-        <li>
-          <Link to="findjobs">BROWSE JOB</Link>
-        </li>
-        <li>
-          <Link to="/jobseeker">CONTACT</Link>
-        </li>
-      </ul>
-      <span style={{fontSize: "2rem"}}>
+    <nav
+      className={
+        navClass +
+        " " +
+        (isLinkActive("/jobseeker/findjobs") ? classes.job : "")
+      }
+    >
+      <div className={classes.left}>
+        <span>Inspiring Go</span>
+        <ul className={classes.items}>
+          <li>
+            <NavLink to="">HOME</NavLink>
+          </li>
+          <li>
+            <NavLink to="findjobs">BROWSE JOB</NavLink>
+          </li>
+          <li>
+            <NavLink to="/jobseeker">CONTACT</NavLink>
+          </li>
+        </ul>
+      </div>
+      <span className={classes.profile}>
         <CgProfile />
       </span>
     </nav>
