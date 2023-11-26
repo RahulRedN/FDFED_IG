b2d2b2682ae9964d87e3e50094784f47118e7717
Rahul <rahul.red1801@gmail.com>
2023-11-26 12:02:58 +0530

    test slick

--- src/components/JobSeeker/TopJobs.jsx
+++ src/components/JobSeeker/TopJobs.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 
+import Slider from "react-slick";
+
 import classes from "./TopJobs.module.css";
 
 const TopJobs = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.info}>
         <span>100+</span>
         <h1>Browse From Our Top Jobs</h1>
       </div>
-      <div className={classes.topJobs}></div>
+      <div className={classes.topJobs}>
+        <Slider {...settings}>
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </Slider>
+      </div>
     </div>
   );
 };
