f46c578d77b94a7b67e3d0f3c818fb8ada1ba3d4
Sravan Krishna Beera <sravankrishna32@gmail.com>
2023-11-27 19:29:47 +0530

    sravan v1

--- src/components/company_components/C_Header.jsx
+++ src/components/company_components/C_Header.jsx
@@ -4,7 +4,7 @@ import classes from "./Company.module.css"
 
 export const Company_Header = () => {
   return (
-    <div className={classes.navbar }>
+    <div className={classes.navbar + " shadow-md "}>
         <div className={classes.logo +" text-xl "}><a href="#">InspiringGO</a></div>
         <div className={classes.nav_elements}>
                 <a href="#">HOME</a>
@@ -13,11 +13,15 @@ export const Company_Header = () => {
                 
         </div>
         <div className={classes.end_elements}>
-          <a href="#" className={classes.highlight}>POST A JOB</a>
-          <a href="#">PROFILE</a>
+          <a href="#" className={classes.highlight + " ring-2 ring-blue-500 hover:shadow-lg"}>POST A JOB</a>
+          <a href="#" className={classes.profile}>PROFILE</a>
         </div>
     </div>
   )
 }
 
 export default Company_Header
+
+// 841600
+
+// 9472
--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -1,10 +1,16 @@
-import Company_Header from "../components/company_components/Company_Header"
+import Company_Header from "../components/company_components/C_Header"
+// import Company_Registration from "../components/company_components/C_Stepper"
+// import { Form1 ,Form2} from "../components/company_components/C_Form"
 import classes from "../components/company_components/Company.module.css"
+
+// import C_Form from "../components/company_components/C_Form"
+import Reg_Form from "../components/registration_form_components/Reg_Form"
 
 const Company = () => {
   return (
     <div className={classes.container}>
       <Company_Header />
+      <Reg_Form />      
     </div>
   )
 }
