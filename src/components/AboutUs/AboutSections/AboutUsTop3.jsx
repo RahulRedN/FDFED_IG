f992f45b884e061ac75b74485925d3338e8e4906
Rahul <rahul.red1801@gmail.com>
2023-11-29 21:54:57 +0530

    counter

--- src/components/AboutUs/AboutSections/AboutUsTop3.jsx
+++ src/components/AboutUs/AboutSections/AboutUsTop3.jsx
@@ -1,30 +1,33 @@
 import "../AboutSections/Styles/AboutUsTop3.css";
 
+import { HeartHandshake, Globe2 } from "lucide-react";
 
-import { HeartHandshake ,Globe2} from 'lucide-react';
+import Counter from "../../UI/Counter";
 
 const AboutUsTop3 = () => {
   return (
     <div className="AboutUsTop3">
-
       <div className="peoples">
         {/* <img src={<HeartHandshake />} alt="" /> */}
         <Globe2 className="Globe2" />
-        <h6>687+</h6>
+        <h6>
+          <Counter from={0} to={687} duration={2} />+
+        </h6>
         <p>People Hired</p>
       </div>
 
       <div className="peoples">
-        <HeartHandshake  className="Globe2"/>
-        <h6>672+</h6>
+        <HeartHandshake className="Globe2" />
+        <h6>
+          <Counter from={0} to={672} duration={2}/>+
+        </h6>
         <p>Satisfied Company</p>
-        <div/>
+        <div />
       </div>
 
-
       <div className="provides">
-          <h3>We Provide Awesome Service</h3>
-        </div>
+        <h3>We Provide Awesome Service</h3>
+      </div>
     </div>
   );
 };
