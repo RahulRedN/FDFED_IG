2dc1a4f8865c612dd6a3d1adaef3cb7bc7f3325c
Rahul <rahul.red1801@gmail.com>
2023-12-02 01:43:04 +0530

    homepage update v0.1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 // import Slideshow from "../components/homepage_compos/Slideshow"
 // import Navbar_home from "../components/homepage_compos/Navbar_home"
-import Slider from "../components/homepage_compos/ImageSlider_Chakra"
-import Roles from "../components/homepage_compos/Roles"
-import Testimonials from "../components/homepage_compos/Testimonials"
-import Footer_Job from "../components/homepage_compos/Footer_Job"
+import Slider from "../components/homepage_compos/ImageSlider_Chakra";
+import Roles from "../components/homepage_compos/Roles";
+import OurServices from "../components/homepage_compos/OurServices";
+import Testimonials from "../components/homepage_compos/Testimonials";
+import Footer_Job from "../components/homepage_compos/Footer_Job";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -11,12 +12,13 @@ import "slick-carousel/slick/slick-theme.css";
 const Home = () => {
   return (
     <>
-      <Slider/>
-      <Roles/>
-      <Testimonials/>
-      <Footer_Job/>
+      <Slider />
+      <Roles />
+      <OurServices />
+      <Testimonials />
+      <Footer_Job />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
