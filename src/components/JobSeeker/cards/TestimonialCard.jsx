5096afd0df97a2b85ef1ecd0a906a65726535a3b
khadar-oothuru <khadaroothuru@gmail.com>
2023-12-07 22:27:18 +0530

    testimonails updated

--- src/components/JobSeeker/cards/TestimonialCard.jsx
+++ src/components/JobSeeker/cards/TestimonialCard.jsx
@@ -1,23 +1,86 @@
+
+// import { IoPersonCircle } from "react-icons/io5";
+// import classes from "../Home/Testimonials.module.css";
+
+// const testimonialData = [
+//   {
+//     id: 1,
+//     quote: "In the realm of UI/UX design, our team is dedicated to crafting seamless and visually appealing user experiences...",
+//     name: "John Doe",
+//     role: "UI/UX Designer",
+//   },
+//   {
+//     id: 2,
+//     quote: "Our development team is committed to building robust and scalable solutions that meet the needs of our clients...",
+//     name: "Jane Smith",
+//     role: "Software Engineer",
+//   },
+//   {
+//     id: 3,
+//     quote: "As a project manager, I focus on ensuring timely delivery and effective communication within the team...",
+//     name: "Bob Johnson",
+//     role: "Project Manager",
+//   },
+//   {
+//     id: 4,
+//     quote: "In the world of marketing, creativity and strategy go hand in hand to create compelling campaigns that resonate with our audience...",
+//     name: "Alice Williams",
+//     role: "Marketing Specialist",
+//   },
+// ];
+
+// const TestimonialCard = () => {
+//   return (
+//     <>
+//       {testimonialData.map((testimonial) => (
+//         <div className={classes.card} key={testimonial.id}>
+//           <div className={classes.info}>
+//             <img src="/assets/quotes-sign.webp" alt="Quotes" />
+//             <p>{testimonial.quote}</p>
+//           </div>
+//           <div className={classes.reviewer}>
+//             <IoPersonCircle size={60} />
+//             <div className={classes.name}>
+//               <span>{testimonial.name}</span>
+//               <p>{testimonial.role}</p>
+//             </div>
+//           </div>
+//         </div>
+//       ))}
+//     </>
+//   );
+// };
+
+// export default TestimonialCard;
+
+
+
+
+
+
+
+
+
+
+// TestimonialCard.jsx
 import React from "react";
-
 import { IoPersonCircle } from "react-icons/io5";
-
 import classes from "../Home/Testimonials.module.css";
 
-const TestimonialCard = () => {
+const TestimonialCard = ({ data }) => {
+  const { quote, name, role } = data;
+
   return (
     <div className={classes.card}>
       <div className={classes.info}>
-        <img src="/assets/quotes-sign.webp" />
-        <p>
-        In the realm of UI/UX design, our team is dedicated to crafting seamless and visually appealing user experiences. We believe that design is not just about aesthetics but also about creating interfaces that users love to engage with. Our designers pay meticulous attention to detail, ensuring that every pixel aligns with the overall user journey.
-        </p>
+        <img src="/assets/quotes-sign.webp" alt="Quotes" />
+        <p>{quote}</p>
       </div>
       <div className={classes.reviewer}>
-        <IoPersonCircle size={60}/>
+        <IoPersonCircle size={60} />
         <div className={classes.name}>
-          <span> Name </span>
-          <p>UI/UX Designer</p>
+          <span>{name}</span>
+          <p>{role}</p>
         </div>
       </div>
     </div>
@@ -25,3 +88,4 @@ const TestimonialCard = () => {
 };
 
 export default TestimonialCard;
+
